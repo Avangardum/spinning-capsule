8d10c70cde0d63084cf53389d15ef3cc84a1bc40
Silvian-73 <yurykuz65@gmail.com>
2021-01-24 16:47:57 +0300

    Сделал прыжок

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class PlayerController : SingletonMonoBehaviour<PlayerController>
 {
+    private const float groundedY = 1.1f;
+    
     public float Speed;
     
     [SerializeField] private float spinTime;
@@ -16,8 +18,12 @@ public class PlayerController : SingletonMonoBehaviour<PlayerController>
     private float _rotationSpeed;
     private bool _isRotating;
     private bool _isTurningTo0 = true;
+    private bool _isJumping;
+    private bool _isTopPointPassed;
     private float _zRotation;
     private float _ditancePassedThisSpin;
+    private float _jumpingTopY;
+    private float _verticalSpeedForThisJump;
 
     private float DesiredZRotation => _isTurningTo0 ? -360 : -180;
     private float DistancePassedPerSpin => Speed * spinTime;
@@ -73,7 +79,59 @@ public class PlayerController : SingletonMonoBehaviour<PlayerController>
             transform.Translate(new Vector3(deltaX, 0, 0), Space.World);
             _ditancePassedThisSpin += deltaX;
         }
-        
+
+        if (_isJumping)
+        {
+            // вертикальное движение
+            if (!_isTopPointPassed)
+            {
+                float yMovementToTopPointLeft = _jumpingTopY - transform.position.y;
+                float thisFrameYMovement = _verticalSpeedForThisJump * Time.fixedDeltaTime;
+                if (thisFrameYMovement < yMovementToTopPointLeft)
+                {
+                    transform.Translate(Vector3.up * thisFrameYMovement, Space.World);
+                }
+                else
+                {
+                    transform.Translate(Vector3.up * yMovementToTopPointLeft + 
+                                        Vector3.down * (thisFrameYMovement - yMovementToTopPointLeft), Space.World);
+                    _isTopPointPassed = true;
+                }
+            }
+            else
+            {
+                float yMovementToGroundLeft = groundedY - transform.position.y;
+                float thisFrameYMovement = -_verticalSpeedForThisJump * Time.fixedDeltaTime;
+                if (thisFrameYMovement > yMovementToGroundLeft)
+                {
+                    transform.Translate(Vector3.up * thisFrameYMovement, Space.World);
+                }
+                else
+                {
+                    transform.Translate(Vector3.up * yMovementToGroundLeft, Space.World);
+                    _isJumping = false;
+                }
+            }
+            
+            // горизонтальное движение
+            transform.Translate(Vector3.right * (Speed * Time.fixedDeltaTime), Space.World);
+            
+            // вращение
+            ZRotation -= 180 * Time.fixedDeltaTime * _verticalSpeedForThisJump > 5 ? 3.7f : 2.85f;
+            if (!_isJumping)
+            {
+                if (ZRotation < -160 && ZRotation > -200)
+                {
+                    ZRotation = -180;
+                    _isTurningTo0 = false;
+                }
+                else
+                {
+                    ZRotation = 0;
+                    _isTurningTo0 = true;
+                }
+            }
+        }
         
         Speed += deltaSpeedPerSecond * Time.fixedDeltaTime;
         if (Speed < minSpeed)
@@ -84,7 +142,7 @@ public class PlayerController : SingletonMonoBehaviour<PlayerController>
 
     private void Update()
     {
-        if (!_isRotating)
+        if (!_isRotating && !_isJumping)
         {
             if (InputManager.Instance.Tap)
             {
@@ -92,6 +150,19 @@ public class PlayerController : SingletonMonoBehaviour<PlayerController>
                 _isTurningTo0 = !_isTurningTo0;
                 _ditancePassedThisSpin = 0;
             }
+
+            if (InputManager.Instance.SwipeUp)
+            {
+                _isJumping = true;
+                _verticalSpeedForThisJump = Speed;
+                _jumpingTopY = groundedY + _verticalSpeedForThisJump / 2;
+                _isTopPointPassed = false;
+            }
         }
+    }
+
+    private float CalculateYWhileJumping(Vector2 top)
+    {
+        return Mathf.Pow(transform.position.x - top.x, 2) + top.y;
     }
 }
